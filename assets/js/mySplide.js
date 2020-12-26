78672b64f2a30eb2405435074a59fda3de68fa76
kola192 <mutwakil96@gmail.com>
2020-12-26 22:44:29 +0200

    Initial commit.

---
+++ assets/js/mySplide.js
@@ -0,0 +1,34 @@
+// Initializing partners casousel
+let partnersSplide = new Splide( '.splide', {
+    perPage: 2,
+    perMove: 1,
+    type: 'loop',
+    width: '100%',
+    pagination: false,
+    autoplay: true,
+    interval: 9000,
+    speed: 500,
+    pauseOnHover: false,
+    direction: 'ttb',
+    heightRatio: .8,
+    gap: '2rem',
+    start: 1,
+    breakpoints: {
+      992: {
+        perPage: 2,
+        heightRatio: 1.2,
+      },
+      768: {
+        perPage: 1,
+        heightRatio: 1.2,
+      },
+      459: {
+        perPage: 1,
+        heightRatio: 1.2,
+        interval: 5000,
+        speed: 500,
+      },
+    }, 
+  } ).mount();
+
+  
